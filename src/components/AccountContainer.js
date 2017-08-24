4207ad8f9c1b9f5adc068f1897b11be36486eca6
Catherine <cmlavin7@gmail.com>
2017-08-24 14:21:04 -0400

    Done.

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import CategorySelector from './CategorySelector'
-import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
   constructor() {
     super()
-    //... your code here
+    this.state = {
+      data: [],
+      selected: 'All'
+    }
   }
 
-  handleChange() {
-    //... your code here
+  handleChange = (category) => {
+    this.setState({
+      selected: category
+    })
+
+
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      data: data
+    }))
   }
 
   render() {
-    console.log(transactions)
+    //console.log(transactions)
+// what do you want me to do with each item from my array
+
+    const transactions = this.state.selected == 'All' ? this.state.data : this.state.data.filter((ele) => {
+      console.log(ele.category)
+      return ele.category == this.state.selected
+      // some truthy value
+    })
     return (
       <div className="ui grid container">
 
-        <CategorySelector />
+        <CategorySelector handleSelect={this.handleChange} categorySelected={this.state.selected}/>
 
-        <TransactionsList />
+        <TransactionsList transactions={transactions}/>
 
       </div>
     )
